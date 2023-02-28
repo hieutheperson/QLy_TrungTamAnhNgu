715c1f003c594c06767d306b643bf1d7fbbf7038
hieutheperson <hieutheperson@gmail.com>
2023-02-28 12:42:28 +0700

    Add main file to project

---
+++ QLy_TrungTamAnhNgu/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLy_TrungTamAnhNgu
+{
+    public partial class F_Login : Form
+    {
+        public F_Login()
+        {
+            InitializeComponent();
+        }
+
+        private void F_Login_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
